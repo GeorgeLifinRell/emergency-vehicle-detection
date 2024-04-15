036346ba754bd7dadc412d6b2e42cc14c61fc6c4
Rell <georgelifinrell@gmail.com>
2024-04-16 00:03:03 +0530

    Json returned from flask
    Clientside fetching yet to be completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from keras.models import load_model
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import librosa
@@ -11,6 +11,9 @@ try:
     print("Model loaded successfully.")
 except Exception as e:
     print("Error loading the model:", e)
+
+# Audio file path
+audio_file_path = './audio_files/fileName.mp3'
 
 def features_extractor(file_name):
     audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
@@ -27,13 +30,21 @@ def test_audio_lstm(audio_file_path, model):
     prediction = model.predict(features)
     # Decode the predicted class
     predicted_class = LabelEncoder.inverse_transform([np.argmax(prediction)])
+    print(predicted_class)
     return predicted_class[0]
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def ambulance_generated():
+    if request.method == 'POST':
+        emergency_vehicle_detected = test_audio_lstm(audio_file_path=audio_file_path, model=model)
+        return jsonify({'emergency_vehicle_detected': emergency_vehicle_detected})
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
